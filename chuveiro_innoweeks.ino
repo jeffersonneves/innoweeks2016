eb8c3dd53736afe48cddedfea6533e90662ef375
Jefferson <jefferson.neves@gmail.com>
2016-08-31 08:28:42 -0300

    Basic flow structure.

--- chuveiro_innoweeks.ino
+++ chuveiro_innoweeks.ino
@@ -1,42 +1,91 @@
+#define FLOWSENSOR 2  // Flow sensor pin
+#define RELAY      7  // Relay pin
+#define LEDPIN    13  // Board led pin
 
-// reading liquid flow rate using Seeeduino and Water Flow Sensor from Seeedstudio.com
-// Code adapted by Charles Gantt from PC Fan RPM code written by Crenn @thebestcasescenario.com
-// http:/themakersworkbench.com http://thebestcasescenario.com http://seeedstudio.com
+volatile bool hasFlow; // flag to indicate flow
+int doIt = 1; // controls the execution of the demo
 
-volatile int NbTopsFan; //measuring the rising edges of the signal
-int Calc;
-int hallsensor = 2;    //The pin location of the sensor
-int relayPin = 7;
-
-void rpm ()     //This is the function that the interupt calls
-{
-  NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal
+void flow () {    // This is the function that the interupt calls
+  hasFlow = true; // If it was called it means that we have flow
 }
 
 // The setup() method runs once, when the sketch starts
-void setup() //
-{
-//  pinMode(hallsensor, INPUT); //initializes digital pin 2 as an input
-//  Serial.begin(9600); //This is the setup function where the serial port is initialised,
-//  attachInterrupt(digitalPinToInterrupt(hallsensor), rpm, RISING); //and the interrupt is attached
+void setup() {
+  hasFlow = false;
 
-  pinMode(relayPin, OUTPUT);
+  pinMode(FLOWSENSOR, INPUT); //initializes digital pin 2 as an input
+  
+  pinMode(RELAY, OUTPUT); //initializes relay pin as output
+  digitalWrite(RELAY, LOW);
+
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, LOW);
+
+  Serial.begin(9600);
+
+  attachInterrupt(digitalPinToInterrupt(FLOWSENSOR), flow, RISING); // the interrupt is attached
+
 }
 
-// the loop() method runs over and over again,
-// as long as the Arduino has power
-void loop ()
-{
-//  NbTopsFan = 0;     //Set NbTops to 0 ready for calculations
-//  sei();             //Enables interrupts
-//  delay (1000);      //Wait 1 second
-//  cli();             //Disable interrupts
-//  Calc = (NbTopsFan * 60 / 7.5); //(Pulse frequency x 60) / 7.5Q, = flow rate in L / hour
-//  Serial.print (Calc, DEC); //Prints the number calculated above
-//  Serial.print (" L/hour\r\n"); //Prints "L/hour" and returns a  new line
-  digitalWrite(relayPin, HIGH);
-  delay(1000);
-  digitalWrite(relayPin, LOW);
-  delay(1000);
+void loop() {
 
+  if (doIt){
+    justDoIt(); // start the process, since it's a single time execution demo, just one pass
+    doIt = 0;
+  }
+
+  digitalWrite(LEDPIN, HIGH); delay (250);
+  digitalWrite(LEDPIN, LOW);  delay (250);
+  digitalWrite(LEDPIN, HIGH); delay (250);
+  digitalWrite(LEDPIN, LOW);  delay (250);
+  digitalWrite(LEDPIN, HIGH); delay (1000);
+  digitalWrite(LEDPIN, LOW);  delay (1000);
+}
+
+void justDoIt () {
+  bool pumpItUp = false;
+
+  while (!hasFlow) {
+    sei();        // Enables interrupts
+    delay (1000); // Wait 1 second
+    cli();        // Disable interrupts
+    Serial.print("hasFlow is: ");
+    Serial.println((hasFlow?"true":"false"));
+  }
+
+  // just checking...
+  if (false) {
+    //postAlarm();
+
+    //pumpItUp = getHANAResponse();
+    while (!pumpItUp) {
+      delay (500);
+      //pumpItUp = getHANAResponse();
+    }
+
+    if (pumpItUp){
+
+      // turn on the pump
+      Serial.println("Liga a bomba");
+      digitalWrite(RELAY, HIGH);
+
+      // wait for 20 seconds
+      delay (20000);
+
+      // turn off the pump
+      Serial.println("Desliga a bomba");
+      digitalWrite(RELAY, LOW);
+    }
+  }
+}
+
+void postAlarm() {
+  Serial.println("Postando Alarme");
+  delay (500);
+}
+
+bool getHANAResponse(){
+  Serial.println("Resposta enviada");
+  delay (5000);
+  return true;
 }
